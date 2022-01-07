42e3fc295b20a8f033deb4451a8d9abf55790d2f
monica <mnabais@advicefront.com>
2022-01-07 16:51:26 +0000

    Created game structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,92 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { "src": "/img/dog1.jpg", matched: false },
+  { "src": "/img/dog2.jpg", matched: false },
+  { "src": "/img/dog3.jpg", matched: false },
+  { "src": "/img/dog4.jpg", matched: false },
+  { "src": "/img/dog5.jpg", matched: false },
+  { "src": "/img/dog6.jpg", matched: false }
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards and add an ID to each of them
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          })
+        })
+        reset();
+      } else {
+        setTimeout(() => reset(), 500);
+      }
+    }
+
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices and increase turn
+  const reset = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+    setDisabled(false);
+  }
+
+  // start a game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, []);
+
   return (
-    <div>
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
